c68168ece6f6a14e542a37543ff0f2a0ccc07887
Orion247 <dallinmolson@gmail.com>
2023-01-07 11:35:42 -0800

    update

---
+++ CSE 310/Lab 01 Hello World/Lab 01 Hello World/Lab 01 Hello World.cpp
@@ -0,0 +1,19 @@
+/*************************************************************
+ * 1. Name:
+ *      Dallin Olson
+ * 2. Assignment Name:
+ *      Lab 01: Hello World
+ * 3. Assignment Description:
+ *      Place the text "Hello world" on the screen.
+ * 4. What was the hardest part? Be as specific as possible.
+ *      Configuring Visual Community.
+ * 5. How long did it take for you to complete the assignment?
+ *      30 min
+ *****************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
